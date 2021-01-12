743fe43a3b99567c557c0e944511427b218fd486
rykerdz <rykerdz18@gmail.com>
2021-01-12 17:09:44 +0100

    added the file

---
+++ times.py
@@ -0,0 +1,18 @@
+import datetime
+from time import sleep
+l=[]
+tym=[]
+
+n=int(raw_input("Enter the no. of numbers to be generated"))
+
+for i in range (n):
+    time=datetime.datetime.now()
+    t=time.microsecond
+
+    l.append(t)
+    sleep(0.112)
+
+for i in (l):
+    tym.append(i/10000)
+
+print tym
